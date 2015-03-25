d2b83437415bb0a3dd63b5b0151b43ff38015c6f
dushka-dragoeva <d.dragoeva@abv.bg>
2015-03-25 14:35:20 +0200

    Upload C sharpII Homeworks Upload OOP/Defining-Classes- Part-Tow-Homework Upload OOP/Defining-Classes-Part-One-Homework upload OOP/Extension-Methods-Delegates-Lambda-LINQ-Homework

---
+++ CSharpII/NumeralSystemsHomework/02. BinaryToDecimal/BinaryToDecimal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Write a program to convert binary numbers to their decimal representation.
+class BinaryToDecimal
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter binary number:  ");
+        string number = Console.ReadLine();
+        Console.WriteLine("Decimal is "+BinaryDecimal(number));
+    }
+
+    static long BinaryDecimal(string bynaryNumber)
+    {
+        long decimalNumber = 0;
+        for (int i = 0; i < bynaryNumber.Length;i++ )
+        {
+            int digit = bynaryNumber[i] - '0';
+            int position =bynaryNumber.Length-i-1;
+            decimalNumber += digit * (long)Math.Pow(2, position);
+        }
+            return decimalNumber;
+    }
+}
+
+
+
---
+++ OOP/Extension-Methods-Delegates-Lambda-LINQ-Homework/GroupedByGroupNumber/Student.cs
@@ -0,0 +1,25 @@
+﻿namespace GroupedByGroupNumber
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Student
+    {
+        public Student( string firstName, string lastName,  int groupNumber)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.GroupNumber = groupNumber;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int GroupNumber { get; set; }
+       
+        public override string ToString()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+        
+    }
+}
